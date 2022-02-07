95b59ee2c6a55c2ef8e508106000156dadf3abdf
Valentina Kuznetsova <val.vashurina@gmail.com>
2022-02-07 14:37:27 +0200

    timer JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-
+//TABS
 const tabs = document.querySelectorAll('.tabheader__item');
 const tabsContent = document.querySelectorAll('.tabcontent');
 const tabsParent = document.querySelector('.tabheader__items');
@@ -38,5 +38,58 @@ tabsParent.addEventListener('click', (evt) => {
         });
     }
 });
+
+//TIMER
+const deadline = '2022-03-22';
+
+function getTimeRemainig(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000*60*60*24));
+    const hours = Math.floor((t / (1000*60*60)) % 24);
+    const minutes = Math.floor((t / (1000*60)) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemainig(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if(t.total <=0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('.timer', deadline);
 
 });
